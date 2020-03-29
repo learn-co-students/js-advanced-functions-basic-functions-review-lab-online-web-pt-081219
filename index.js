02010dcec7dcd5db2bb6c05f632dae80e3187058
umberto palazzo <upalazzo93@gmail.com>
2020-03-29 12:35:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(param = "special"){
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+let Calculator = {
+    add(num1, num2){
+        return num1 + num2
+    },
+
+    subtract(num1, num2){
+        return num1 - num2
+    },
+
+    multiply(num1, num2){
+        return num1 * num2
+    },
+
+    divide(num1, num2){
+        return num1/num2
+    }
+}
+
+function actionApplyer(num, collection= []){
+    for (let i = 0; i<collection.length; i++){
+        num = collection[i](num);
+    };
+    return num
+}
+    
